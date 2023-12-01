a8626d189b5c5a2f0b434daabe1b4c003be2e336
yutaro.hatsuda <yutaro.hatsuda@webcrew.co.jp>
2023-12-01 14:36:55 +0900

    add:通常のStackOverFlow例を追加

---
+++ src/main/scala/StackOverFlowExample.scala
@@ -0,0 +1,18 @@
+import scala.annotation.tailrec
+
+@main
+def main = {
+  println("StackOverFlow test:Fibonacci")
+  println(getFibonacciNaive(1000000))
+}
+
+
+def getFibonacciNaive(index: BigInt): BigInt = {
+  if (index <= 0) {
+    0
+  } else if (index == 1) {
+    1
+  } else {
+    getFibonacciNaive(index - 1) + getFibonacciNaive(index - 2)
+  }
+}
