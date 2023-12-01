876c84abedfce4866ab24f7e53a22eff2a99efe9
yutaro.hatsuda <yutaro.hatsuda@webcrew.co.jp>
2023-12-01 17:48:08 +0900

    update:不要かっこ削除

--- src/main/scala/StackOverFlowExample.scala
+++ src/main/scala/StackOverFlowExample.scala
@@ -1,18 +1,15 @@
 import scala.annotation.tailrec
 
 @main
-def main = {
+def main(): Unit =
   println("StackOverFlow test:Fibonacci")
   println(getFibonacciNaive(1000000))
-}
 
 
-def getFibonacciNaive(index: BigInt): BigInt = {
-  if (index <= 0) {
+def getFibonacciNaive(index: BigInt): BigInt =
+  if (index <= 0)
     0
-  } else if (index == 1) {
+  else if (index == 1)
     1
-  } else {
+  else
     getFibonacciNaive(index - 1) + getFibonacciNaive(index - 2)
-  }
-}
